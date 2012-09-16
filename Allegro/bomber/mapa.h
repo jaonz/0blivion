0cf6a06249c10e318f7d1309e7b2900e9a9d16b2
GustavoKatel <gustavobs.katel@gmail.com>
2012-09-15 23:30:42 -0300

    bomber. mapa.h (defines)

--- Allegro/bomber/mapa.h
+++ Allegro/bomber/mapa.h
@@ -1,8 +1,8 @@
 #ifndef MAPA_H
 #define MAPA_H
 
-#define MAPA_LARGURA 300
-#define MAPA_ALTURA 300
+#define MAPA_LARGURA 640
+#define MAPA_ALTURA 480
 
 extern int mapa[MAPA_LARGURA][MAPA_ALTURA];
 

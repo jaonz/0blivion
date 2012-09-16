ce05827a841a3cbcb0e2024032ec6b2d0cec4433
GustavoKatel <gustavobs.katel@gmail.com>
2012-09-15 22:27:56 -0300

    Bomber - mapa (.h/.c), makefile.

---
+++ bomber/mapa.c
@@ -0,0 +1,3 @@
+#include "mapa.h"
+
+int mapa[MAPA_LARGURA][MAPA_ALTURA];
---
+++ bomber/mapa.h
@@ -0,0 +1,9 @@
+#ifndef MAPA_H
+#define MAPA_H
+
+#define MAPA_LARGURA 300
+#define MAPA_ALTURA 300
+
+extern int mapa[MAPA_LARGURA][MAPA_ALTURA];
+
+#endif
